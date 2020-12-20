7c3dfec0f4b874b5d8f25a15fd40f823075bccbd
moshiuzzaman <md.shozon041@gmail.com>
2020-12-20 10:18:39 +0600

    first commit

---
+++ src/Component/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuBar from '../Menubar/MenuBar';
+
+const Home = () => {
+    return (
+        <div>
+            <MenuBar/>
+        </div>
+    );
+};
+
+export default Home;
